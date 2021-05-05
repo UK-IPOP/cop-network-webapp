fb16b559e8b5281481f3bf11e788c6d42ec35101
Nick Anthony <nanthony007@gmail.com>
2021-05-05 11:27:02 -0400

    Add IPOP graph support.

--- app/utils.py
+++ app/utils.py
@@ -18,3 +18,21 @@ def load_graph_from_files() -> tuple[nx.Graph, nx.layout]:
     with open("data/cop-graph-positions.pkl", "rb") as f:
         positions = pickle.load(f)
     return g, positions
+
+
+def save_ipop_graph(connections: list[tuple[str, str]]):
+    G = nx.Graph()
+    G.add_edges_from(connections)
+    positions = nx.spring_layout(G)
+    with open("data/ipop-graph.pkl", "wb") as f:
+        pickle.dump(G, f)
+    with open("data/ipop-graph-positions.pkl", "wb") as f:
+        pickle.dump(positions, f)
+
+
+def load_ipop_graph_from_files() -> tuple[nx.Graph, nx.layout]:
+    with open("data/ipop-graph.pkl", "rb") as f:
+        g = pickle.load(f)
+    with open("data/ipop-graph-positions.pkl", "rb") as f:
+        positions = pickle.load(f)
+    return g, positions
