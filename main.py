747907f6176cd14655a7577f11eadafe280e7904
Nick Anthony <nanthony007@gmail.com>
2021-05-06 12:43:58 -0400

    Add tabs for COP and IPOP separate graphs.

--- main.py
+++ main.py
@@ -19,13 +19,19 @@ from app import graphing, utils
 # TODO: performance improvements
 
 
-def load_scholar_names_from_file() -> list[str]:
+def load_scholar_names_from_file(ipop_only: bool = False) -> list[str]:
     with open("data/COPscholars.csv", "r") as f:
         csvreader = csv.DictReader(f)
         authors = []
-        for row in csvreader:
-            authors.append(row.get("Name"))
-    return authors
+        if ipop_only:
+            for row in csvreader:
+                if row.get('Group') == 'Pharmacy Practice and Science':
+                    authors.append(row.get("Name"))
+            return authors
+        else:
+            for row in csvreader:
+                authors.append(row.get("Name"))
+            return authors
 
 def create_cop_network_graph_figure():
     graph, positions = utils.load_graph_from_files()
@@ -68,46 +74,20 @@ app = dash.Dash(__name__, title="COP Scholar Network Dashboard", external_styles
 server = app.server
 
 scholar_names = load_scholar_names_from_file()
+ipop_names = load_scholar_names_from_file(ipop_only=True)
 cop_network_graph = create_cop_network_graph_figure()
 ipop_network_graph = create_ipop_network_graph_figure()
 
 
-app.layout = dbc.Container(
+tab1 = dbc.Container(
     [
-        dbc.Row(
-            [
-                dbc.Col(
-                    [
-                        html.Img(
-                            src="/assets/IPOP-logo.png",
-                            style={'width': '100px'},
-                        )
-                    ],
-                    align="center",
-                    width=2,
-                ),
-                dbc.Col([html.H1(children="UK COP Scholarship Network", className="text-info", style={'text-align': 'center'})], width=4),
-                dbc.Col(
-                    [
-                        html.Img(
-                            src="/assets/UK-COP-logo.jpg",
-                            style={'width': '250px', 'height': '200px'},
-                        )
-                    ],
-                    align="center",
-                    width=2,
-                ),
-            ],
-            justify="center",
-            align="center",
-        ),
         dbc.Row(
             [
                 dbc.Col(
                     [
                         html.H2('Description:', className='text-center text-info'),
                         html.Hr(),
-                        html.P(
+                        html.P([
                             "This network graph shows authors and their direct coauthors. "
                             "When an author is selected you are able to see the author's entire network graph. "
                             "When you select two authors, you are able to see their combined network(s) and any "
@@ -115,8 +95,10 @@ app.layout = dbc.Container(
                             "are shown, and any other authors are only showcasing a sub-graph or sub-network of their "
                             "entire network. To see their entire network, selected them from the dropdown. If they "
                             "are not in the dropdown, then you can request to add them, although at this time only "
-                            "COP scholars are included. There are 111 COP scholars/authors available to choose from."
-                        ),
+                            "COP scholars are included. There are: ",
+                            html.Span(f"{len(scholar_names)} COP ", className='strong text-primary'),
+                            "scholars/authors available to choose from."
+                        ]),
                     ],
                     width=9,
                 )
@@ -170,11 +152,70 @@ app.layout = dbc.Container(
             justify="center",
             align="center",
         ),
+    ],
+    fluid=True
+)
+
+
+tab2 = dbc.Container(
+    [
+        dbc.Row([
+            dbc.Col(
+                [
+                    html.H2('Description:', className='text-center text-info'),
+                    html.Hr(),
+                    html.P([
+                        "This network graph shows authors and their direct coauthors. "
+                        "When an author is selected you are able to see the author's entire network graph. "
+                        "When you select two authors, you are able to see their combined network(s) and any "
+                        "shared connections they may have. Note that only full graphs for the selected authors "
+                        "are shown, and any other authors are only showcasing a sub-graph or sub-network of their "
+                        "entire network. To see their entire network, selected them from the dropdown. If they "
+                        "are not in the dropdown, then you can request to add them, although at this time only "
+                        "COP scholars are included. There are: ",
+                        html.Span(f"{len(ipop_names)} IPOP ", className='strong text-primary'),
+                        "scholars/authors available to choose from."
+                    ]),
+                ],
+                width=9,
+                )
+            ],
+            justify="center",
+            align="center",
+        ),
+        dbc.Row(
+            [
+                dbc.Col(
+                    [
+                        html.Label("Author 1 Select:", className="text-info"),
+                        dcc.Dropdown(
+                            id="author-dropdown3",
+                            options=[],
+                            value="",
+                        ),
+                    ],
+                    width=4,
+                ),
+                dbc.Col(
+                    [
+                        html.Label("Author 2 Select:", className="text-info"),
+                        dcc.Dropdown(
+                            id="author-dropdown4",
+                            options=[],
+                            value="",
+                        ),
+                    ],
+                    width=4,
+                ),
+            ],
+            justify="center",
+            align="center",
+        ),
         dbc.Row(
             [
                 dbc.Card(
                     dbc.Spinner(
-                        dcc.Graph(figure=ipop_network_graph),
+                        dcc.Graph(figure=ipop_network_graph, id='ipop-graph'),
                         type="grow",
                         color="primary",
                         size="lg",
@@ -187,8 +228,59 @@ app.layout = dbc.Container(
             className="px-5",
             justify="center",
             align="center",
-            id='ipop-container',
         ),
+    ],
+    fluid=True
+)
+
+main_content = dbc.Card(
+    [
+        dbc.CardHeader(
+            dbc.Tabs(
+                [
+                    dbc.Tab(label="COP", tab_id="tab-1", tabClassName='ml-auto'),
+                    dbc.Tab(label="IPOP", tab_id="tab-2", tabClassName='mr-auto'),
+                ],
+                id="card-tabs",
+                card=True,
+                active_tab="tab-1",
+            )
+        ),
+        dbc.CardBody(id='main_content_body'),
+    ]
+)
+
+app.layout = dbc.Container(
+    [
+        dbc.Row(
+            [
+                dbc.Col(
+                    [
+                        html.Img(
+                            src="/assets/IPOP-logo.png",
+                            style={'width': '100px'},
+                        )
+                    ],
+                    align="center",
+                    width=2,
+                ),
+                dbc.Col([html.H1(children="UK COP Scholarship Network", className="text-info", style={'text-align': 'center'})], width=4),
+                dbc.Col(
+                    [
+                        html.Img(
+                            src="/assets/UK-COP-logo.jpg",
+                            style={'width': '250px', 'height': '200px'},
+                        )
+                    ],
+                    align="center",
+                    width=2,
+                ),
+            ],
+            justify="center",
+            align="center",
+        ),
+        main_content
+
     ],
     fluid=True,
 )
@@ -219,6 +311,30 @@ def update_options2(input_value: str) -> list[dict[str, str]]:
 
 
 @app.callback(
+    Output(component_id="author-dropdown4", component_property="options"),
+    Input(component_id="author-dropdown3", component_property="value"),
+)
+def update_options3(input_value: str) -> list[dict[str, str]]:
+    return [
+        {"label": parse_name(person), "value": person}
+        for person in ipop_names
+        if person != input_value
+    ]
+
+
+@app.callback(
+    Output(component_id="author-dropdown3", component_property="options"),
+    Input(component_id="author-dropdown4", component_property="value"),
+)
+def update_options4(input_value: str) -> list[dict[str, str]]:
+    return [
+        {"label": parse_name(person), "value": person}
+        for person in ipop_names
+        if person != input_value
+    ]
+
+
+@app.callback(
     Output("network-graph", "figure"),
     [
         Input(component_id="author-dropdown1", component_property="value"),
@@ -232,17 +348,26 @@ def on_author_select(author1: Union[str, None], author2: Union[str, None]) -> go
 
 
 @app.callback(
-    Output("ipop-container", "style"),
+    Output("ipop-graph", "figure"),
     [
-        Input(component_id="author-dropdown1", component_property="value"),
-        Input(component_id="author-dropdown2", component_property="value"),
+        Input(component_id="author-dropdown3", component_property="value"),
+        Input(component_id="author-dropdown4", component_property="value"),
     ],
 )
-def hide_graph(author1: Union[str, None], author2: Union[str, None]) -> dict[str, str]:
+def draw_ipop_graph(author1: Union[str, None], author2: Union[str, None]) -> go.Figure:
     if author1 or author2:
-        return {'display': 'none'}
+        return pair_graph(author1, author2)
+    return ipop_network_graph
+
+
+@app.callback(
+    Output("main_content_body", "children"), [Input("card-tabs", "active_tab")]
+)
+def tab_content(active_tab):
+    if active_tab == "tab-2":
+        return tab2
     else:
-        return {'display': 'block'}
+        return tab1
 
 
 if __name__ == "__main__":
